474957d21e81a1cf27612c774af933915ba87cf6
Werfran <franciscozufiaurre@gmail.com>
2023-01-18 02:11:32 -0300

    Se agregaron datos de contacto

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,7 @@ fetch("https://randomuser.me/api/").then(
 fillData = function(dataUsuario){
     document.getElementById("imagenUsuario").src = dataUsuario.picture.large
     document.getElementById("nombreHeader").innerHTML = dataUsuario.name.last + ', ' + dataUsuario.name.first+'.';
+    document.getElementById("correoText").innerHTML = dataUsuario.email;
+    document.getElementById("telefonoText").innerHTML = dataUsuario.phone;
+    document.getElementById("celularText").innerHTML = dataUsuario.cell ;
 }
