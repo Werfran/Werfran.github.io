70497253f36d6f382e0ce72522371229b40de9ae
Werfran <franciscozufiaurre@gmail.com>
2023-01-18 02:47:35 -0300

    Se agrego copiado de campos

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,15 @@ fillData = function(dataUsuario){
     document.getElementById("nombreHeader").innerHTML = dataUsuario.name.last + ', ' + dataUsuario.name.first+'.';
     document.getElementById("correoText").innerHTML = dataUsuario.email;
     document.getElementById("telefonoText").innerHTML = dataUsuario.phone;
-    document.getElementById("celularText").innerHTML = dataUsuario.cell ;
+    document.getElementById("celularText").innerHTML = dataUsuario.cell;
+    document.getElementById("residenciaText").innerHTML = dataUsuario.location.city+', '+ dataUsuario.location.state + ', '+ dataUsuario.location.country;
+}
+
+function bufferText(fieldName) {    
+    const el = document.createElement("textarea");
+    el.value = document.getElementById(fieldName).innerHTML
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
 }
