371bb57555d124718f886929abfe6835b912e9e1
Werfran <franciscozufiaurre@gmail.com>
2023-01-18 23:08:53 -0300

    mejoras en la lista de contactos

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
-fetch("https://randomuser.me/api/").then(
-    (response) => {
-        const responseObj= response.json();
-        responseObj.then((data)=> {
-            const persona = data.results[0];
-            localStorage.setItem("usuarioData",JSON.stringify(persona));
-            fillData(persona);
-        })
-    }
-);
+fetch("https://randomuser.me/api/").then((response) => {
+    const responseObj = response.json();
+    responseObj.then((data) => {
+        const persona = data.results[0];
+        localStorage.setItem("usuarioData", JSON.stringify(persona));
+        fillData(persona);
+    });
+});
 
-fillData = function(dataUsuario){
-    document.getElementById("imagenUsuario").src = dataUsuario.picture.large
-    document.getElementById("nombreHeader").innerHTML = dataUsuario.name.last + ', ' + dataUsuario.name.first+'.';
+fillData = function (dataUsuario) {
+    document.getElementById("imagenUsuario").src = dataUsuario.picture.large;
+    document.getElementById("nombreHeader").innerHTML =
+        dataUsuario.name.last + ", " + dataUsuario.name.first + ".";
     document.getElementById("correoText").innerHTML = dataUsuario.email;
     document.getElementById("telefonoText").innerHTML = dataUsuario.phone;
     document.getElementById("celularText").innerHTML = dataUsuario.cell;
-    document.getElementById("residenciaText").innerHTML = dataUsuario.location.city+', '+ dataUsuario.location.state + ', '+ dataUsuario.location.country;
-}
+    document.getElementById("residenciaText").innerHTML =
+        dataUsuario.location.city +
+        ", " +
+        dataUsuario.location.state +
+        ", " +
+        dataUsuario.location.country +
+        ".";
+};
 
-function bufferText(fieldName) {    
+function bufferText(fieldName) {
     const el = document.createElement("textarea");
-    el.value = document.getElementById(fieldName).innerHTML
+    el.value = document.getElementById(fieldName).innerHTML;
     document.body.appendChild(el);
     el.select();
     document.execCommand("copy");
